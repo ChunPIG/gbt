020a23b463d90a3d7f13c44e3181b7b0343084e6
zhengcongchun <zhengcongchun@airag.cn>
2017-11-02 17:41:21 +0800

    add user repository test

---
+++ gbt-dao/src/test/java/com/fitt/gbt/dao/UserRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.fitt.gbt.dao;
+
+/**
+ * <p>@description: Repository接口测试类</p>
+ * <p>@copyright: Copyright(C) 2017 by AIRAG</p>
+ * <p>@author: Chuck[ZhengCongChun]</p>
+ * <p>@created: 2017-11-02</p>
+ * <p>@version: 1.0</p>
+ */
+public class UserRepositoryTest {
+}
